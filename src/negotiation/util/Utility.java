daabb367bb0fda4272ef342960ab4322dce1e748
sondremare <sondremo@gmail.com>
2015-02-27 22:21:15 +0100

    added utility calculations and nextBid values

---
+++ src/negotiation/util/Utility.java
@@ -0,0 +1,34 @@
+package negotiation.util;
+
+import java.util.ArrayList;
+
+public class Utility {
+
+    /** The buyers utility is multiplied with 2, because the most desired state is to own all wanted items */
+    public int getBuyersUtility(Item item) {
+        return 2 * item.getValue();
+    }
+
+    public int getSellersUtility(Item item) {
+        return item.getValue();
+    }
+
+    /** Gets the buyers next bid based on a conceeder strategy **/
+    public static int getBuyersNextBid(ArrayList<Item> wishList, int remainingMoney, Item item, int time, int totalTime) {
+        int recommendedPriceForAllWishedItems = 0;
+        for (Item wishedItem : wishList) {
+            recommendedPriceForAllWishedItems += wishedItem.getValue();
+        }
+        double maxBidFactor = (double)(recommendedPriceForAllWishedItems / remainingMoney);
+        double buyersMinimumBid = 0;
+        double buyersMaximumBid = maxBidFactor * item.getValue();
+        return (int)(buyersMinimumBid + (buyersMaximumBid - buyersMinimumBid) * Math.pow((double)(time/totalTime), 1/Math.E));
+    }
+
+    /** Gets the sellers next bid based on a boulware strategy **/
+    public static int getSellersNextBid(Item item, int time, int totalTime) {
+        int buyersMinimumBid = (int)(0.5 * item.getValue());
+        int buyersMaximumBid = 2 * item.getValue();
+        return (int)(buyersMinimumBid + (buyersMaximumBid - buyersMinimumBid) * Math.pow(1 - (double)(time/totalTime), 1/Math.E));
+    }
+}
